3d04718d5cbef46eb294699d8dd8effe7b369790
Showy <>
2020-10-23 18:23:31 +0900

    [add]deploy for heroku
    added Procfile and deleted ActiveAdmin to deploy for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,5 @@ Rails.application.routes.draw do
     end
   end
   
-  ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
