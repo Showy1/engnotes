2150b3ab49f1fb10fd71f1446f2f1491509a0fcd
Showy <>
2020-11-08 14:28:50 +0900

    [add] Font Awesome, header and footer
    added Font Awesome, header and footer to design UI/UX

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,5 +7,6 @@ require('@rails/ujs').start();
 require('@rails/activestorage').start();
 require('channels');
 import 'bootstrap';
+import '@fortawesome/fontawesome-free/js/all';
 import '../src/application.scss';
 const images = require.context('../images/', true); // eslint-disable-line
