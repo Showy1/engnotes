513335fcb8ba4272b5c34f26cae3209b365d6d4a
Showy <>
2020-11-09 17:41:00 +0900

    [update] flash auto fadeout
    activated flash fadeout with jQuery fadeOut

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,19 @@
-const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
-const eslint =  require('./loaders/eslint')
+const { environment } = require('@rails/webpacker');
+const { VueLoaderPlugin } = require('vue-loader');
+const vue = require('./loaders/vue');
+const eslint =  require('./loaders/eslint');
+const webpack = require('webpack');
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-environment.loaders.append('eslint', eslint)
-module.exports = environment
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin());
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    Popper: 'popper.js/dist/popper'
+  })
+);
+environment.loaders.prepend('vue', vue);
+environment.loaders.append('eslint', eslint);
+module.exports = environment;
