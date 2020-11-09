faa72d7b9ccf09e8bc4956a0fefe2511f06aab4d
Showy <>
2020-11-09 17:41:00 +0900

    [update] flash auto fadeout
    activated flash fadeout with jQuery fadeOut

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,14 @@
 require('@rails/ujs').start();
 require('@rails/activestorage').start();
 require('channels');
+import 'jquery';
 import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all';
 import '../src/application.scss';
 const images = require.context('../images/', true); // eslint-disable-line
+
+$(function(){
+  setTimeout(function(){
+    $('.flash').fadeOut(1000);
+  }, 5000);
+});
